246f2d7e224bde1e46b701b2883a8aae66e97ad5
ARLeader <Bolshave_K@hotmail.com>
2014-07-08 13:39:44 +0900

    Change to Column wise Mat store

--- ConsoleApplication1/Tester.cpp
+++ ConsoleApplication1/Tester.cpp
@@ -13,6 +13,7 @@
 using namespace cv;
 using namespace std;
 
+/*Create new windows displaying image of a given matrix.*/
 int displayImage(Mat image)
 {
 	if (!image.data)
@@ -26,11 +27,18 @@ int displayImage(Mat image)
 
 }
 
+/*Load an image from a given path and return with a matrix of the image.*/
 Mat readImage(string path, int format)
 {
 	Mat image;
+	try
+	{
 	image = imread(path, format);
-
+	}
+	catch (int e)
+	{
+		cout << "An exception occurred. Exception Nr. " << e << '\n';
+	}
 	return image;
 }
 
@@ -40,36 +48,48 @@ void printMatrix(Mat matrix)
 	cout << matrix << endl;
 }
 
+
 Mat convertMatToVector(Mat matrix)
 {
-	Mat row = matrix.reshape(1, 1);
+	Mat row = matrix.t();
+	row = row.reshape(1, 1);
 	return row;
 }
 
+/*Load image(s) specified by given filePath in form of vectors.*/
 Mat loadBatchImages(string filePath)
 {
-	ifstream fileStream;
 	string line;
 	Mat coAll, tmp;
-	coAll = Mat::ones(224, 1, CV_8U); //Row vector of 224,1
-	int imgNum = 0;
-
-	fileStream.open(filePath, std::ifstream::in);
-	if (fileStream.is_open()) 
+	try
 	{
-		while (getline(fileStream, line)){
-			hconcat(coAll, convertMatToVector(readImage(line, 0)).t(), coAll);
+		ifstream fileStream;
+		
+		coAll = Mat::ones(224, 1, CV_8U); //Create a column of 1 for using with concat.
+		int imgNum = 0;
+
+		fileStream.open(filePath, std::ifstream::in);
+		if (fileStream.is_open())
+		{
+			while (getline(fileStream, line)){
+				hconcat(coAll, convertMatToVector(readImage(line, 0)).t(), coAll);
+			}
+			coAll = coAll.colRange(1, coAll.cols); //Remove first column vector.
 		}
-		coAll = coAll.colRange(1, coAll.cols);
+		else{
+			cout << "ERROR OPENING FILES";
+		}
+		fileStream.close();
 	}
-	else{
-		cout << "ERROR OPENING FILES";
+	catch (int e)
+	{
+		cout << "An exception occurred. Exception Nr. " << e << '\n';
 	}
-	fileStream.close();
-	//import from path
 	return coAll;
+	
 }
 
+/*OBSOLETE return mean vector column of given matrix*/
 Mat findMeanVector(Mat matrix)
 {
 
@@ -79,22 +99,52 @@ Mat findMeanVector(Mat matrix)
 
 }
 
+/*Write a given image to a file in the given path*/
+void writeMatToFile(Mat matrix,string filePath)
+{
+	try
+	{
+		ofstream fileStream;
+		fileStream.open(filePath);
+		fileStream << matrix;
+		fileStream.close();
+
+	}
+	catch (int e)
+	{
+		cout << "An exception occurred. Exception Nr. " << e << '\n';
+	}
+}
+
+
 int main(int argc, char** argv)
 {	
 	string dir = "C:\\faceimg\\01\\DIR.txt";
 	Mat face01 = loadBatchImages(dir);
 	
+	writeMatToFile(face01, "C:\\faceimg\\01\\FACE01.txt");
 	
-	ofstream fileStream;
-	fileStream.open("C:\\faceimg\\01\\FACE01.txt");
-	fileStream << face01;
+	
+	Mat a = readImage("C:\\faceimg\\01\\31.pgm",0);
+	imwrite("C:\\faceimg\\01\\a.pgm", a);
+	Mat av = convertMatToVector(a);
+	imwrite("C:\\faceimg\\01\\av.pgm", av);
 
-
-	Mat covar01,mean01,eigen01;
-	mean01 =(face01);
+	Mat covar01,mean01,eigenvec01,eigenval01,nCovar01;
 	calcCovarMatrix(face01, covar01, mean01, CV_COVAR_COLS, CV_64F);
-	eigen(covar01, eigen01, -1, -1);
-	waitKey(0);
+	eigen(covar01, eigenval01,eigenvec01, -1, -1);
+	normalize(covar01, nCovar01, 1, 0, NORM_MINMAX, -1, Mat());
+	writeMatToFile(nCovar01, "C:\\faceimg\\01\\NCOVAR01.txt");
+	writeMatToFile(covar01, "C:\\faceimg\\01\\COVAR01.txt");
+	writeMatToFile(eigenval01, "C:\\faceimg\\01\\EIGENVAL01.txt");
+	writeMatToFile(eigenvec01, "C:\\faceimg\\01\\EIGENVEC01.txt");
+	
+	int numOfComponent = 64;
+	PCA pca(face01, noArray(), CV_PCA_DATA_AS_COL, numOfComponent);
+	Mat pcaEigenval01 = pca.eigenvalues.clone();
+	Mat pcaEigenvec01 = pca.eigenvectors.clone();
+	writeMatToFile(pcaEigenval01, "C:\\faceimg\\01\\PCAEIGENVAL01.txt");
+	writeMatToFile(pcaEigenvec01, "C:\\faceimg\\01\\PCAEIGENVEC01.txt");
 	return(0);
 }
 
