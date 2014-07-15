f4b77e15a757bed61d5df72308a541e040317bc5
ARLeader <Bolshave_K@hotmail.com>
2014-07-15 15:56:32 +0900

    Implement Face Recognizer

--- ConsoleApplication1/Tester.cpp
+++ ConsoleApplication1/Tester.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
 	Class	:	Tester
 	Author	:	Lattasit Haritaworn
 	Date	:	2014/06/27
@@ -6,14 +6,16 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
 
-int MAXIMUM_COMPONENTS_NUMBER = 3;
-
 using namespace cv;
 using namespace std;
+
+int MAXIMUM_COMPONENTS_NUMBER = 3;
+Size DEFAULT_IMAGE_SIZE(100, 100);
 
 /*Create new windows displaying image of a given matrix.*/
 int displayImage(Mat image)
@@ -36,18 +38,13 @@ Mat readImage(string path, int format)
 	try
 	{
 		image = imread(path, format);
+		resize(image, image, DEFAULT_IMAGE_SIZE);
 	}
 	catch (int e)
 	{
 		cout << "An exception occurred. Exception Nr. " << e << '\n';
 	}
 	return image;
-}
-
-void printMatrix(Mat matrix)
-{
-	  
-	cout << matrix << endl;
 }
 
 /*Load image(s) specified by given filePath in form of vectors.*/
@@ -59,14 +56,14 @@ Mat loadBatchImages(string filePath)
 	{
 		ifstream fileStream;
 		
-		coAll = Mat::ones(1, 224, CV_8U); //Create a row of 1 for using with concat.
+		coAll = Mat::ones(1, DEFAULT_IMAGE_SIZE.height * DEFAULT_IMAGE_SIZE.width, CV_8U); //Create a row of 1 for using with concat.
 		int imgNum = 0;
 
 		fileStream.open(filePath, std::ifstream::in);
 		if (fileStream.is_open())
 		{
 			while (getline(fileStream, line)){
-				vconcat(coAll, readImage(line, 0).reshape(1,1), coAll);
+				vconcat(coAll, readImage(line, CV_LOAD_IMAGE_GRAYSCALE).reshape(1, 1), coAll);
 			}
 			coAll = coAll.rowRange(1, coAll.rows); //Remove first row vector.
 		}
@@ -92,7 +89,7 @@ void loadBatchImages(const string& filePath, Mat& images, Mat& labels, char sepa
 	{
 		ifstream fileStream;
 		
-		images = Mat::ones(1, 224, CV_8U); //Create a row of 1 for using with concat.
+		images = Mat::ones(1, DEFAULT_IMAGE_SIZE.height * DEFAULT_IMAGE_SIZE.width, CV_8U); //Create a row of 1 for using with concat.
 		int imgNum = 0;
 
 		fileStream.open(filePath, std::ifstream::in);
@@ -104,7 +101,7 @@ void loadBatchImages(const string& filePath, Mat& images, Mat& labels, char sepa
 				getline(liness, path, separator);
 				getline(liness, classlabel);
 				if (!path.empty() && !classlabel.empty()) {
-					vconcat(images, readImage(path, 0).reshape(1,1), images);
+					vconcat(images, readImage(path, CV_LOAD_IMAGE_GRAYSCALE).reshape(1,1), images);
 					labels.push_back(atoi(classlabel.c_str()));
 				}
 			}
@@ -160,6 +157,10 @@ void initMassPCA(Mat faces, vector<int> label,vector<PCA>& pcas)
 		int labelCount = 1, dataStart = 0, faceCount = 0;
 		for (int i = 0; i < label.size(); i++)
 		{
+
+
+			cout << "Initiate PCA of Person No. " << i << endl;
+
 			if (label.at(i) != labelCount)
 			{
 				
@@ -174,43 +175,84 @@ void initMassPCA(Mat faces, vector<int> label,vector<PCA>& pcas)
 
 }
 
+/*Return result of Face Recognition of a testImage*/
+int FaceRecog(Mat testImage, vector<PCA> PCAs)
+{
+	Mat coeff = Mat::ones(1, MAXIMUM_COMPONENTS_NUMBER, CV_32F);
+	Mat coeffSum;
+	float coeffMax = 0;
+	int PCAcount = 1, coeffMaxIndex, coeffMinIndex;
+	for each (PCA pca in PCAs)
+	{
+		vconcat(coeff, pca.project(testImage), coeff);
+		PCAcount++;
+	}
+
+	coeff = coeff.rowRange(1, coeff.rows);
+	multiply(coeff, coeff, coeffSum);
+	reduce(coeffSum, coeffSum, 1, CV_REDUCE_SUM, -1);
+
+	for (int i = 0; i < coeffSum.rows; i++)
+	{
+		if (coeffSum.at<float>(i) > coeffMax)
+		{
+			coeffMax = coeffSum.at<float>(i);
+			coeffMaxIndex = i;
+		}
+	}
+
+
+	return coeffMaxIndex;
+}
+
+/*Report Result of Face Recognition on every faces provided*/
+void testFaceRecog(Mat faces, vector<PCA> pcaFaces, vector<int> label)
+{
+	int result;
+	double correctCount = 0;
+	Mat recognizerCountMet = Mat::zeros(pcaFaces.size(), 1, CV_16S), recognizerCorrectMet = Mat::zeros(pcaFaces.size(), 1, CV_16S);
+	vector<int> recognizerCount = recognizerCountMet, recognizerCorrect = recognizerCorrectMet;
+
+	for (int testFaceNum = 0; testFaceNum < faces.rows; testFaceNum++)
+	{
+		result = FaceRecog(faces.row(testFaceNum), pcaFaces);
+		cout << "The face Number " << testFaceNum << " has been recognized as Face Number : " << result << endl;
+		if (result == label.at(testFaceNum))
+		{
+			cout << "PONG! ";
+			recognizerCorrect[label.at(testFaceNum)]++;
+			correctCount++;
+		}
+		recognizerCount[result]++;
+	}
+
+
+	cout << endl << "The Result is Here" << endl;
+	int count = 0;
+	for (int i = 0; i < recognizerCount.size(); i++)
+	{
+		cout << "Face Number : " << count << "\tHas the Score of : " << recognizerCount[i] << "\tWith " << recognizerCorrect[i] << " Correct Rate." << endl;
+		count++;
+	}
+	cout << "This program has correctly recognize " << correctCount << " faces" << endl << "Out of " << faces.rows << endl << "Which is calculated as " << (correctCount / faces.rows) * 100 << " Percents." << endl;
+
+}
+
 int main(int argc, char** argv)
 {	
-	string dir = "C:\\faceimg\\01\\DIR.txt";
-	string csvdir = "C:\\faceimg\\faceDIR.txt";
+	string csvdir = "C:\\camface\\faceDIR.txt";
 	string outdir = "C:\\faceout\\";
-	//Mat face01 = loadBatchImages(dir);
+	
 	Mat label;
 	Mat faces;
 	loadBatchImages(csvdir, faces, label, ';');
-	writeMatToFile(faces, outdir + "faces.txt");
-	writeMatToFile(label, outdir + "label.txt");
+	//writeMatToFile(faces, outdir + "faces.txt");
+	//writeMatToFile(label, outdir + "label.txt");
 
 	vector<PCA> pcaFaces;
 	initMassPCA(faces,label,pcaFaces);
 
-	PCA testPCA = pcaFaces.back();
-	writeMatToFile(testPCA.eigenvectors, outdir + "PCAEIGEN31.TXT");
-
-	/*Mat covar01,mean01,eigenvec01,eigenval01,nCovar01;
-	calcCovarMatrix(face01, covar01, mean01, CV_COVAR_ROWSS | CV_COVAR_NORMAL | CV_COVAR_SCALE, CV_64F);
-	eigen(covar01, eigenval01,eigenvec01, -1, -1);
-	normalize(covar01, nCovar01, 1, 0, NORM_MINMAX, -1, Mat());
-	writeMatToFile(nCovar01, "C:\\faceimg\\01\\NCOVAR01.txt");
-	writeMatToFile(covar01, "C:\\faceimg\\01\\COVAR01.txt");
-	writeMatToFile(eigenval01, "C:\\faceimg\\01\\EIGENVAL01.txt");
-	writeMatToFile(eigenvec01, "C:\\faceimg\\01\\EIGENVEC01.txt");
-	*/
-
-	
-	/*
-	int numOfComponent = 0;
-	PCA pca(face01, noArray(), CV_PCA_DATA_AS_COL, numOfComponent);
-	Mat pcaEigenval01 = pca.eigenvalues.clone();
-	Mat pcaEigenvec01 = pca.eigenvectors.clone();
-	writeMatToFile(pcaEigenval01, "C:\\faceimg\\01\\PCAEIGENVAL01.txt");
-	writeMatToFile(pcaEigenvec01, "C:\\faceimg\\01\\PCAEIGENVEC01.txt");
-	*/
+	testFaceRecog(faces, pcaFaces, label);
 	
 	return(0);
 	
